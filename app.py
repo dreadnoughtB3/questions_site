4f761391eafa7c2d824d0037087c8d2066f6a939
dreadnoughtB3 <dreadnoughtx3@gmail.com>
2023-12-14 00:39:22 +0900

    product ver

--- app.py
+++ app.py
@@ -25,20 +25,53 @@ authenticator.login('ログイン', 'main')
 # ログイン済み
 if st.session_state["authentication_status"]:
     questions_prog = user_data[st.session_state["name"]]
-    if questions_prog == 4:
+    if questions_prog == 11:
         prog_text = "*COMPLETED*"
     else:
-        prog_text = f"*{questions_prog}/3*"
+        prog_text = f"*{questions_prog}/10*"
     st.write('*Created by mayonaka4355*')
     st.write(f'>ログイン中のユーザー：*{st.session_state["name"]}*  \n>現在の問題進捗　　　：{prog_text}')
     st.divider()
     
-    if questions_prog == 4:
-        st.write("`全問題終了。イベント本番をお待ちください。`")
-    elif st.session_state["name"] == "mayonaka":
+    if st.session_state["name"] == "mayonaka":
         st.json(correct_answerer)
+        
+        json_string_1 = json.dumps(correct_answerer, indent=2, ensure_ascii=False)
+        json_string_2 = json.dumps(user_data, indent=2, ensure_ascii=False)
+        
+        st.download_button(
+            label="correct_answerer.json",
+            file_name="correct_answerer.json",
+            mime="application/json",
+            data=json_string_1,
+        )
+        st.download_button(
+            label="user_data.json",
+            file_name="user_data.json",
+            mime="application/json",
+            data=json_string_2,
+        )
+        try:
+            if authenticator.register_user('ユーザー追加', preauthorization=False):
+                with open('./config.yaml', 'w') as file:
+                    yaml.dump(config, file, default_flow_style=False)
+                st.success('User registered successfully')
+        except Exception as e:
+            st.error(e)
+        new_user_name = st.text_input('新規ユーザー名')
+        if st.button("追加"):
+            if new_user_name in user_data:
+                st.warning("既にユーザー名が登録されています")
+            else:
+                user_data[new_user_name] = 1
+                print(user_data)
+                with open('data/user_data.json', 'w', encoding="utf-8") as f:
+                    json.dump(user_data, f, indent=2, ensure_ascii=False)
+        
+    elif questions_prog == 11:
+        st.write("`全問題終了。お疲れさまでした。`")
     else:
-        st.write(f"> ◖問題[{questions_prog}/3]")
+        st.write(f"> ◖問題[{questions_prog}/10]")
         question_data = questions["questions"][str(questions_prog)]
         st.write(question_data)
         
