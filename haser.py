2f5a83dd1c8092f6d9b9553bc38c5e682d6f88ba
dreadnoughtB3 <dreadnoughtx3@gmail.com>
2023-12-14 00:51:34 +0900

    password fix

--- haser.py
+++ haser.py
@@ -1,3 +1,3 @@
 import streamlit_authenticator as stauth
-hashed_passwords = stauth.Hasher(['basashi', 'herusere', '12f310d13t']).generate()
+hashed_passwords = stauth.Hasher(['F95Fs4hTyCwd']).generate()
 print(hashed_passwords)
