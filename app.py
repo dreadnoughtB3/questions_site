06dfd0636d08f9aabc8469beebf0b2b12022e85e
dreadnoughtB3 <dreadnoughtx3@gmail.com>
2023-12-13 21:16:33 +0900

    add datetime

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 import json
+import datetime
 from time import sleep
 from yaml.loader import SafeLoader
 
@@ -9,7 +10,7 @@ with open('./config.yaml', 'rb') as file:
     config = yaml.load(file, Loader=SafeLoader)
 questions = json.load(open('data/questions.json', 'r', encoding="utf-8"))
 user_data = json.load(open('data/user_data.json', 'r', encoding="utf-8"))
-
+correct_answerer = json.load(open('data/correct_answerer.json', 'r', encoding="utf-8"))
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -34,6 +35,8 @@ if st.session_state["authentication_status"]:
     
     if questions_prog == 4:
         st.write("`全問題終了。イベント本番をお待ちください。`")
+    elif st.session_state["name"] == "mayonaka":
+        st.json(correct_answerer)
     else:
         st.write(f"> ◖問題[{questions_prog}/3]")
         question_data = questions["questions"][str(questions_prog)]
@@ -44,8 +47,13 @@ if st.session_state["authentication_status"]:
         if st.button("回答する"):
             if inputText_A == questions["answers"][str(questions_prog)]:
                 user_data[st.session_state["name"]] += 1
-                with open('data/user_data.json', 'w') as f:
-                    json.dump(user_data, f, indent=2)
+                with open('data/user_data.json', 'w', encoding="utf-8") as f:
+                    json.dump(user_data, f, indent=2, ensure_ascii=False)
+                current_time = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
+                correct_answerer[str(questions_prog)][st.session_state["name"]] = {"name":st.session_state["name"], "date":str(current_time)}
+                with open('data/correct_answerer.json', 'w', encoding="utf-8") as file:
+                    sleep(1)
+                    json.dump(correct_answerer, file, indent=2, ensure_ascii=False)
                 st.warning('正解')
                 st.balloons()
                 sleep(2)
